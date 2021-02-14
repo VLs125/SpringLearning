df1f091958eb2a2701394658589e55b8150c0073
Vladislav Stalmakhovich <VLs125.25@gmail.com>
2021-02-14 22:36:09 +1000

    one to one in hibernate

---
+++ src/main/java/hibernate_test2/Test1.java
@@ -0,0 +1,66 @@
+package hibernate_test2;
+
+import hibernate_test2.entity.Detail;
+import hibernate_test2.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.util.List;
+
+public class Test1 {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .addAnnotatedClass(Detail.class)
+                .buildSessionFactory();
+//        try {
+//            Session session = factory.getCurrentSession();
+//            Employee emp = new Employee("Violetta", "Kucher","Sales",3000);
+//            Detail detail = new Detail("Vladivostok","91431251064","test2@email.ru");
+//            session.beginTransaction();
+//
+//            emp.setEmpDetail(detail);
+//
+//            session.save(emp);
+//
+//            session.getTransaction().commit();
+//
+//
+//
+//
+//
+//            System.out.println("Done!");
+//
+//        }
+        try {
+            Session session = factory.getCurrentSession();
+
+            session.beginTransaction();
+
+            Employee emp = session.get(Employee.class,3);
+
+            System.out.println(emp);
+
+
+
+            session.getTransaction().commit();
+
+
+
+
+
+            System.out.println("Done!");
+
+        }
+
+        finally {
+            factory.close();
+        }
+
+
+
+
+    }
+}
---
+++ src/main/java/hibernate_test2/Test2.java
@@ -0,0 +1,73 @@
+package hibernate_test2;
+
+import hibernate_test2.entity.Detail;
+import hibernate_test2.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Test2 {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .addAnnotatedClass(Detail.class)
+                .buildSessionFactory();
+        Session session = null;
+//Bi-directional one to one Detail and Employee
+        try {
+//            session = factory.getCurrentSession();
+//
+//            Employee emp = new Employee("Zina", "Zotova","HR",2500);
+//            Detail detail = new Detail("Irkutsk","9123762634","test4@email.ru");
+//
+//            detail.setEmployee(emp);
+//            emp.setEmpDetail(detail);
+//
+//            session.beginTransaction();
+//
+//            session.save(detail);
+//
+//            session.getTransaction().commit();
+//
+//            System.out.println("Done!");
+
+            // Получение информации о работнике
+//            session = factory.getCurrentSession();
+//
+//            session.beginTransaction();
+//
+//            Detail detail = session.get(Detail.class,1);
+//
+//            System.out.println(detail.getEmployee());
+//
+//            session.getTransaction().commit();
+//
+//            System.out.println("Done!");
+
+            session = factory.getCurrentSession();
+
+            session.beginTransaction();
+
+            Detail detail = session.get(Detail.class,1);
+
+            session.delete(detail);
+
+            session.getTransaction().commit();
+
+            System.out.println("Done!");
+
+
+
+        }
+
+        finally {
+            session.close();
+            factory.close();
+        }
+
+
+
+
+    }
+}
